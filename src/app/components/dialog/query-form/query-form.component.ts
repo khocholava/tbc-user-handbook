3348a6a6719e5485c4041eb8b1c9c32be73d01b2
akakikhotcholava <akaki.khotcholava@magticom.ge>
2020-12-14 04:36:35 +0400

    need form array bug fix

--- src/app/components/dialog/query-form/query-form.component.ts
+++ src/app/components/dialog/query-form/query-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-query-form',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QueryFormComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,30 +1,47 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {Actions, ofActionSuccessful, Select, Store} from '@ngxs/store';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Actions, Select, Store} from '@ngxs/store';
 import {UserSelectors} from '../../store/user/user.selectors';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {User} from '../../models/user';
 import {QueryUsers, RemoveUser} from '../../store/user';
-import {MatPaginator} from '@angular/material/paginator';
+import {MatPaginator, PageEvent} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatDialog} from '@angular/material/dialog';
 import {DialogComponent} from '../dialog/dialog/dialog.component';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {TranslocoService} from '@ngneat/transloco';
+import {MatSort, Sort} from '@angular/material/sort';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserQueryParams} from '../../store/user/user.types';
+import {FormControl, FormGroup} from '@angular/forms';
+import {filter, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
 })
-export class TableComponent implements OnInit, AfterViewInit {
+export class TableComponent implements OnInit, AfterViewInit, OnDestroy {
   @Select(UserSelectors.getUsers)
   users$: Observable<Array<User>> | undefined;
+  param: UserQueryParams = {};
+  @Select(UserSelectors.getTotalCount)
+  totalCount$: Observable<number>;
+
+  formGroup = this.createFormGroup();
+  subscription = new Subscription();
   dataSource = new MatTableDataSource<User>();
   matRowDef: Array<string> = [
     'image', 'firstName', 'lastName', 'phoneNumber',
     'legalAddress.country', 'actions'
   ];
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  pageSize = 5;
+  pageSizeOptions = [5, 10, 15];
+  pageIndex = 1;
+  pageLimit;
 
   constructor(
     readonly store: Store,
@@ -32,34 +49,39 @@ export class TableComponent implements OnInit, AfterViewInit {
     private actions$: Actions,
     private matSnackbar: MatSnackBar,
     private translocoService: TranslocoService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {
-    this.actions$.pipe(
-      ofActionSuccessful(RemoveUser)
-    ).subscribe(() => {
-      this.store.dispatch(new QueryUsers()).subscribe(() => {
-        this.matSnackbar.open(
-          this.translocoService.translate('messages.successfullyDeleted'),
-          this.translocoService.translate('close'),
-          {
-            duration: 2000,
-            horizontalPosition: 'end',
-            verticalPosition: 'bottom',
-          }
-        );
-      });
-    });
   }
 
   ngAfterViewInit() {
-    // this.dataSource.paginator = this.paginator;
+    this.dataSource.paginator = this.paginator;
   }
 
   ngOnInit(): void {
-    this.store.dispatch(new QueryUsers());
+    this.activatedRoute.queryParams.subscribe(route => {
+      if (route.page) {
+        this.pageIndex = route.page;
+      }
+      if (route.pageSize) {
+        this.pageSize = route.pageSize;
+      }
+    });
+    this.onSearchChange();
+    // this.queryData();
+  }
+
+  createFormGroup() {
+    return new FormGroup({
+      search: new FormControl('')
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   edit(row) {
-    console.log(row);
     this.dialog.open(DialogComponent, {
       width: '50%',
       data: row
@@ -73,6 +95,61 @@ export class TableComponent implements OnInit, AfterViewInit {
   openAddDialog() {
     this.dialog.open(DialogComponent, {
       width: '50%'
+    });
+  }
+
+  queryData() {
+
+    this.store.dispatch(new QueryUsers(
+      {_page: this.pageIndex, _limit: this.pageSize, ...this.param}));
+
+  }
+
+  onSearchChange() {
+    this.formGroup.controls.search.valueChanges.pipe(
+      tap(data => {
+        this.param = {
+          firstName: data
+        };
+        this.pageIndex = 1;
+        this.queryData();
+      }),
+      filter(data => data === ''),
+      tap(() => {
+        this.param = {};
+        this.pageIndex = 1;
+        this.queryData();
+      })
+    ).subscribe();
+  }
+
+  onSortChange($event: Sort) {
+    if ($event) {
+      this.param = {
+        _sort: $event.active,
+        _order: $event.direction,
+      };
+    }
+    this.pageIndex = 1;
+    this.queryData();
+
+    this.router.navigate(['/'], {
+      queryParams: {
+        direction: $event.active,
+        order: $event.direction
+      }
+    });
+  }
+
+  onPageChange($event: PageEvent) {
+    this.pageSize = $event.pageSize;
+    this.pageIndex = $event.pageIndex + 1;
+    this.queryData();
+    this.router.navigate(['/'], {
+      queryParams: {
+        page: $event.pageIndex + 1,
+        pageSize: $event.pageSize
+      }
     });
   }
 }
